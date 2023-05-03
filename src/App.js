d555bb7e05aa40d43ff3162dd8b018bae8bf8bc0
anikatahsin12 <bushratahsin653@gmail.com>
2023-05-04 00:26:09 +0600

    set up all files completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from "./pages";
 import "./App.css";
 
 function App() {
@@ -23,10 +43,12 @@ function App() {
           </div>
           {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              Sidebar
+              <Sidebar />
             </div>
           ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
           )}
           <div
             className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
@@ -34,28 +56,35 @@ function App() {
             }`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              Navbar
+              <Navbar />
             </div>
           </div>
           <div>
             <Routes>
-              {/* Dashboard */}
-              <Route path="/" element="ECommerce" />
-              <Route path="/1" element="ECommerce" />
-              <Route path="/2" element="ECommerce" />
-              <Route path="/3" element="ECommerce" />
+              {/* dashboard  */}
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
 
-              {/* Pages1 */}
-              <Route path="/orders1" element="Pages" />
-              <Route path="/orders2" element="Pages" />
-              <Route path="/orders3" element="Pages" />
-              <Route path="/orders3" element="Pages" />
+              {/* pages  */}
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
 
-              {/* Pages2 */}
-              <Route path="/orders21" element="Pages" />
-              <Route path="/orders22" element="Pages" />
-              <Route path="/orders23" element="Pages" />
-              <Route path="/orders23" element="Pages" />
+              {/* apps  */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+
+              {/* charts  */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
             </Routes>
           </div>
         </div>
---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartsHeader = () => {
+  return <div>Chart Header</div>;
+};
+
+export default ChartsHeader;
